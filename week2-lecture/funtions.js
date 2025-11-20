0651f2c35ab674a15eebb120a3cc95bbeceaea0d
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2025-11-20 14:14:21 +0100

    commmitted

---
+++ week2-lecture/funtions.js
@@ -0,0 +1,91 @@
+/*
+    FUNCTIONS
+    Function these are reusable blocksof code. they are defined and calld any time on the apllication.
+    Syntax
+    function functionName(parameters) {
+        code block
+    declaration of a fuction simply means defining it. stating the name, the code blocks and the parameters.
+
+    calling a function:
+    this is simply invoking the function
+
+    parameters: the variables place holders specified when defining a function
+    while argument are values that are passed when callin the function.
+}
+*/
+
+/* Defining function (Function Declearation)
+function Myname() {
+  console.log("my name is obinna");
+}
+
+//calling function
+Myname();
+
+function Add(a, b) {
+  console.log(a + b);
+}
+
+Add(4, 3);
+Add(80, 56);
+*/
+
+/*Funtion expresion: declearing a function as a variable
+const greet = function (time) {
+  console.log(`Good ${time}`);
+};
+
+greet("morning");
+greet("evenng");
+*/
+
+/*
+  Anonymous function
+  are funion that do not have a name , rather the can be asigned to variable
+
+*/
+
+/*
+Arrow function they make use of arrow. it came from ES6. Only decleared using const
+const Subtract = (c, d) => {
+  console.log(c - d);
+};
+
+Subtract(20, 5);
+*/
+
+/*
+  CALLBACK FUNCTION 
+  this is function that is passed as an argument to another function
+  callback function are used to handle asynchronous operation
+  they ensure that a function is not exectuted before a task is completed but will be exectuted right after the task is completed.
+
+function SayHi(name, welcome) {
+  console.log(`Hello ${name}`);
+  welcome();
+}
+
+function welcome() {
+  console.log("Welcome to the world of JavaScript");
+}
+
+SayHi("Obinna", welcome);
+*/
+
+/*
+    RETURN STATEMENTS
+    return passes a value back from a function to the place where the function was called.
+    it also terminates the function execution
+
+    function Multiply(x, y) {
+      return x * y;
+    }
+
+*/
+
+function Multiply(b, c, d) {
+  return b * c + d;
+}
+
+console.log(Multiply(1, 3, 4));
+console.log(Multiply(5, 6, 7));
--- week2-lecture/lecture-day-2.js
+++ week2-lecture/lecture-day-2.js
@@ -1,7 +1,23 @@
 /* CONTROL STRUCTURE 
     Switch Case
-*/
-
+        1. it is used to perform different actions based on different conditions.
+        2. it is alternative to if...else if...else statement.
+        3. it is used when we have multiple conditions to check.
+        4. it is more readable and easier to maintain than if...else if...else statement.
+            SYNTAX:
+                switch(expression) {
+                    case value1:
+                        code to be executed if expression === value1
+                        break;
+                    case value2:
+                        code to be executed if expression === value2
+                        break;
+                    ...
+                    default:
+                        code to be executed if expression doesn't match any case
+                }
+        */
+/*
 let day = "Wednesday";
 switch (day) {
   case "Monday":
@@ -15,4 +31,95 @@ switch (day) {
     break;
   default:
     console.log("Today is any other day");
+}
+*/
+
+/* LOOPS
+    1. Loops are used to repeat a block of code multiple times until a certain condition is met.
+    2. Types of loops in JavaScript:
+        a. for loop
+        b. while loop
+        c. do...while loop
+*/
+
+/*
+ for loop:
+        used when the number of iterations is known.
+        initialization: it is used to initialize the loop counter variable
+        condition: it is used to check the condition before each iteration.
+        increment/decrement: it is used to update the loop counter variable after each iteration.
+        i++ : increment by 1 (postfix i.e post increament)
+        i-- : decrement by 1 (postfix i.e post decreament)
+        --i : decrement by 1 (prefix i.e pre decreament)
+        ++i : increment by 1 (prefix i.e pre increament)
+
+
+        SYNTAX:
+            for(initialization; condition; increment/decrement) {
+                code to be executed
+            }
+
+
+
+for (let i = 0; i < 6; i++) {
+  console.log("Iteration number: " + i);
+}
+
+for (let b = 8; b > 0; b--) {
+  console.log("Decrementing Iteration number: " + b);
+}
+*/
+
+/*
+WHILE LOOP
+    1. while loop is used when the number of iterations is not known.
+    2. This execute a block of code untill the condition becomes false.
+    SYNTAX
+        while (condition){
+            statements to be executed
+        }
+
+
+let count = 7;
+while (count > 3) {
+  console.log(`Count is : ${count}`);
+  count--;
+}
+*/
+
+/*
+DO WHILE LOOP
+    do while loop: it executes a code atleast once before checking the condiions
+
+    SYNTAX:
+        do {
+        code execution; 
+        iteration;
+        } whil (condiion);
+
+
+let num = 5;
+do {
+  console.log(`num is ${num}`);
+  num++;
+} while (num < 5);
+*/
+
+/* 
+    PRACTICE EXERCISES
+    
+    JAVASCRIPT VALIDATION
+    
+    MULTIPLICATION TABLE
+    trim() method is a string method used for clearing white spaces from the starting and ending of a string.
+*/
+
+let number = prompt("Enter a number : ");
+if (number.trim() == "" || isNaN(number)) {
+  alert("invalid input");
+} else {
+  for (let i = 1; i <= 12; i++) {
+    number = Number(number);
+    console.log(`${number} x ${i} = ${number * i}`);
+  }
 }
